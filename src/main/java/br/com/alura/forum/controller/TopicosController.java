09be7502e4b8c9ca663fa845df1392946b312bbf
Daniel Lopes <viana.daniel02@gmail.com>
2020-07-21 18:02:09 -0300

    Simplificando paginação(@@EnableSpringDataWebSupport) e colocando valores 'padrão'.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -35,9 +37,8 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
-    public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd){
-
-        Pageable paginacao = PageRequest.of(pagina, qtd);
+    public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso,
+     @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page=0, size=10) Pageable paginacao){
 
         if(nomeCurso == null){
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
