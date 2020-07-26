93b983c9660fc020ec32303756ae6b5d6b69a5ee
Daniel Lopes <viana.daniel02@gmail.com>
2020-07-26 14:33:26 -0300

    Gerando token com JWT e retornando p/ cliente

---
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -0,0 +1,35 @@
+package br.com.alura.forum.config.security;
+
+import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${forum.jwt.expiration}")
+    private String expiration;      //Valor "injetado" lá do arquivo application.properties
+
+    @Value("${forum.jwt.secret}")
+    private String secret;      //Valor "injetado" lá do arquivo application.properties
+
+    public String gerarToken(Authentication authentication) {
+
+        Usuario logado = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("API do Fórum da Aulra")         //Quem fez a geração do token.
+                .setSubject(logado.getId().toString())      //Quem é o usuário autenticado que esse token pertence.
+                .setIssuedAt(hoje)                          //Data de criação do token.
+                .setExpiration(dataExpiracao)               //Data de expiração do token, no caso, em milisegundos.
+                .signWith(SignatureAlgorithm.HS256, secret) //Algoritmo p/ encriptografar, e sua respectiva chave.
+                .compact();
+    }
+}
